592bd607b442cd29a885187bc5d1d23b164b538d
Sivabharathi <vssivabharathi@gmail.com>
2024-02-28 11:21:56 +0530

    Inital_cmd

---
+++ 28_feb/01.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a=0;
+    float b=0;
+ for (b=100;b>0;b=b/10){
+     printf(" %f\n",b);
+ }
+
+    return 0;
+}
